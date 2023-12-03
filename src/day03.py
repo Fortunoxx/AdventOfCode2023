e3369a8ca44fbbcfc424cc7d9bf2526d7ff3f822
Fortunoxx <robert_goetz@hotmail.com>
2023-12-03 21:59:35 +0100

    code coverage

--- src/day03.py
+++ src/day03.py
@@ -10,17 +10,16 @@ def get_values(fileInfo):
             for x in range(0, len(line)):
                 if line[x].isnumeric():
                     current += line[x]
-                elif current != "":
-                    pos = (x - len(current), y) # start coordinates
-                    data[pos] = current
-                    current = ""
+                
+                if not line[x].isnumeric() or x == (len(line) - 1):
+                    if current != "":
+                        pos = (x - len(current), y) # start coordinates
+                        data[pos] = current
+                        current = ""
+
                 if not line[x].isnumeric() and line[x] != ".":
                     symbols.append((x, y))
 
-            # if we are at the end of the line...
-            if current != "":
-                pos = (x - len(current), y) # start coordinates
-                data[pos] = current
             y += 1
     return (data, symbols)
 
@@ -28,7 +27,6 @@ def get_values(fileInfo):
 def filter(data, symbols):
     filtered = []
     for d in data:
-        found = False
         (x, y) = d
         for xi in range(x-1, x + len(data[d]) + 1):
             for yi in range(y-1, y+2):
