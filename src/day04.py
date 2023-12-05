a87315305d315df854eefe218e8a0118db75deaa
Fortunoxx <robert_goetz@hotmail.com>
2023-12-05 14:56:08 +0100

    day 04 part 2

--- src/day04.py
+++ src/day04.py
@@ -6,9 +6,29 @@ def get_values(fileInfo, part2=False):
             line = line.replace("\n", "")
             parts = line.split(":")
             card = int(parts[0].replace('Card ', ''))
-            parts2 = parts[1].replace('  ', ' 0').split(" | ")
-            data[card] = { "winning": parts2[0].split(' '), "numbers": parts2[1].split(' ') }
+            parts2 = parts[1].strip().replace('  ', ' 0').split(" | ")
+            winning =  [int(n) for n in parts2[0].split(' ')]
+            numbers =  [int(n) for n in parts2[1].split(' ')]
+            data[card] = { "counter": 1, "winning": winning, "numbers": numbers }
+
     return data
+
+
+def calculate_cards(data):
+    for card_id in data:
+        winning = data[card_id]["winning"]
+        numbers = data[card_id]["numbers"]
+        counter = data[card_id]["counter"]
+
+        cnt = 0
+        for n in numbers:
+            if n in winning:
+                cnt += 1
+
+        for id in range(card_id + 1, card_id + 1 + cnt):
+            data[id]["counter"] = data[id]["counter"] + counter
+
+    return [data[processed]["counter"] for processed in data]
 
 
 def calculate_winning_numbers(data):
@@ -35,5 +55,5 @@ def solve_part1(fileInfo):
 
 def solve_part2(fileInfo):
     data = get_values(fileInfo)
-    result = calculate_winning_numbers(data)
+    result = calculate_cards(data)
     return sum(result)
